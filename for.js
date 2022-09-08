c967c9e99c86d1768f1dc72a1eea9b201b9f1fb4
MissDine <enidnyatichi1212@gmail.com>
2022-09-08 11:55:06 +0300

    Feature edit

---
+++ for.js
@@ -0,0 +1,7 @@
+// Three stages: initialiation(variable declaration),condition,final expression,statement 
+let name = ["Maosa","Moma","Moraa","Marube"]
+function LoopThrough(){
+    for(){
+        // statement
+    }
+}
