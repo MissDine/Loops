bf9907f9859ae1bd182e7d03d3f725f1b99a8d93
MissDine <enidnyatichi1212@gmail.com>
2022-09-08 14:37:19 +0300

    Feature edit

---
+++ while.js
@@ -0,0 +1,11 @@
+// 
+// let countup = 0;
+// while (countup <= 10) {
+//   console.log(countup++);
+// }
+
+let countdown = 10
+while(countdown >= 0){
+    console.log(countdown--);
+}
+console.log("Happy birthdat beb");
